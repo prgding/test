c5b941e0fc06060f9c2adc42bc4a9fd33f7d8319
ding <69354087+prgding@users.noreply.github.com>
2023-03-08 14:27:28 +0800

    init6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("He11111122llo wor111ld!");
+		System.out.println("He11111122222llo wor111ld!");
 	}
 }
